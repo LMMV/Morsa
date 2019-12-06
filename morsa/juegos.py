be7c33c4693c17f9647aefc2c8b7a644f7572bf9
Liliana M. Moreno Vargas <lm.moreno.vargas@gmail.com>
2019-12-06 15:59:24 -0600

    primer intento de creacion de lista y diccionario

---
+++ morsa/juegos.py
@@ -0,0 +1,10 @@
+
+
+lista_de_juegos = ['parchis', 'ajedrez', 'domino', 'tres en raya']
+
+dict_puntos_por_juego={'parchís' : {'gana' : +1, 'pierde' : -1}
+                       'ajedrez' : {'gana' : +2, 'pierde' : -1}
+                       'domino'  : {'gana' : +1, 'pierde' :  0}
+                       'tres en raya' : {'gana' : +1, 'pierde' : -2}
+                      }
+
